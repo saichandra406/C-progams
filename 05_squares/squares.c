dbeae577a117d1eefc7893ef9f9443a9fd56184e
Coursera Learner <nobody@nowhere.nul>
2020-10-18 16:21:37 +0000

    Did assignment 5

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -8,7 +8,7 @@
  */
 int isInRange(int coord, int offset, int size) {
   // if coord is in range, return 1
-  if(coord >= offset && coord < size)
+  if(coord >= offset && coord < offset + size)
   {
     return 1;
   }
@@ -57,14 +57,14 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     //count from 0 to w. Call the number you count with x
     for(int x=0; x < w; x++){
     //check if  EITHER
-      //    ((x is between x_offset  and x_offset +size2) AND 
+      //    ((x in range between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
       //  OR
-      //    ((y is between y_offset and y_offset + size2) AND
+      //    ((y in range between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
     
-      if((x > x_offset && x < xoff_sum && (y == y_offset || y == yoff_sum - 1))
-	 || (y > y_offset && y < yoff_sum && (x == x_offset || x == xoff_sum - 1)))
+      if((x >= x_offset && x < xoff_sum && (y == y_offset || y == yoff_sum - 1))
+	 || (y >= y_offset && y < yoff_sum && (x == x_offset || x == xoff_sum - 1)))
       {	
       // if so, print a *
 	printf("*");
