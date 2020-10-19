034f64c385687b14f8f0006a325f2f4574ed3ca7
saichandra406 <70030168+saichandra406@users.noreply.github.com>
2020-10-19 15:30:28 +0530

    Update retirement.c

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -8,4 +8,14 @@ struct retire_info_t {
 };
 typedef	struct retire_info_t retire_info;
 
+void retirement (int startAge,   //in months
+                 double initial, //initial savings in dollars
+                 retire_info working, //info about working
+                 retire_info retired) //info about being retired
+{}
+
+int main(void) {
+
+}
+
  
