5af8c8be82996e6f335d179f4abca15ebb21b2a0
Coursera Learner <nobody@nowhere.nul>
2020-11-12 17:02:37 +0000

    did assignment 16

--- 16_subseq/maxSeq.c
+++ 16_subseq/maxSeq.c
@@ -7,11 +7,12 @@ size_t maxSeq(int * array, size_t n){
   for(size_t i=1;i < n; i++){
     if(array[i] > array[i-1]){
       tmp++;
+      continue;
     }
     else if(tmp > len){
       len = tmp;
-      tmp = 1;
     }
+    tmp = 1;
   }
 
   return len > tmp? len : tmp;
