3bdb934dcf6637f4588d1039c0d78fe10d8f14d1
Coursera Learner <nobody@nowhere.nul>
2020-11-15 16:09:08 +0000

    did assignment 22

---
+++ 22_tests_power/test-power.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+unsigned power (unsigned x, unsigned y);
+
+int main(void){
+  unsigned x[] = {0, 0, 1, 2, 100};
+  unsigned y[] = {0, 1, 2, 2, 3};
+  unsigned ans[] = {1, 0, 1, 4, 1000000};
+  unsigned tmp;
+  size_t n = *(&x + 1) - x;
+  for (size_t i=0; i < n; i++){
+    tmp = power(x[i], y[i]);
+    printf("%lu - %u\n", i,tmp);
+    if(tmp != ans[i])
+      exit(EXIT_FAILURE);
+  }
+  return EXIT_SUCCESS;
+}
