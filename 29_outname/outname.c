eb544235b646592c0610ebd960c5360a77100966
Coursera Learner <nobody@nowhere.nul>
2020-11-29 04:22:01 +0000

    did assignment 29

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -4,5 +4,16 @@
 #include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  size_t len = strlen(inputName);
+  char * out = NULL;
+  if(len > 0){
+    //out string to append ".counts" with null character, so 7
+    out = malloc((len + 7) * sizeof(*out));
+    if(out != NULL){
+      strcpy(out, inputName);
+      strcat(out, ".counts");
+    }
+  }
+  //default else returns NULL
+  return out;
 }
