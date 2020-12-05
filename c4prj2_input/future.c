bb8aba9afd9083a6f658b7662512ba2d8336a6ac
Coursera Learner <nobody@nowhere.nul>
2020-12-05 15:57:10 +0000

    did assignment c4pjr2

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -12,17 +12,17 @@ int isEmptyCard(card_t * p){
     return 0;
 }
 
-void copyCard(card_t * dest, card_t src){
+void copyCard(card_t * dest, card_t * src){
   dest->suit = src->suit;
   dest->value = src->value;
 }
 
 void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
-  deck_t *cur_deck = fc->decks[index];
-  if(isEmptyCard(cur_deck->cards[0])){
+  deck_t cur_deck = fc->decks[index];
+  if(isEmptyCard(cur_deck.cards[0])){
     //current cards are not replaced.
-    for(size_t j = 0; j < cur_deck->n_cards; j++){
-      copyCard(cur_deck->cards[j], ptr);
+    for(size_t j = 0; j < cur_deck.n_cards; j++){
+      copyCard(cur_deck.cards[j], ptr);
     }
   }
   //if its not empy card do nothing
@@ -38,7 +38,7 @@ void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
   }
   else{
     for(size_t i = 0; i < deck->n_cards; i++){
-      add_future_card(fc, i,)
+      add_future_card(fc, i,deck->cards[i]);
 	}
   }
 }
