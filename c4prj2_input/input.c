b14123a2f2fe7ec0bf63d427dcd72fbf3a600ca3
Coursera Learner <nobody@nowhere.nul>
2020-12-07 11:26:33 +0000

    did assignment cppjr2

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -9,51 +9,37 @@
 #include "future.h"
 #include "input.h"
 
-void stripNewLine(char * line){
-  char *ptr = line;
-  while(*ptr != '\n'){
-    ptr++;
-  }
-  *ptr = '\0';
-}
-
-char * getsub(const char * str, size_t idx){
-  char *ans = NULL;
-  int n = 0;
-  while(str[idx+n] != '\0' && str[idx+n] != ' '){
-    n++;
-  }
-  if(n){
-    ans = malloc((n+1) * sizeof(char));
-    strncpy(ans, str+idx, n);
-    ans[n] = '\0';
-  }
-  return ans;
-}
-
 deck_t * hand_from_string(const char * str, future_cards_t * fc){
-  size_t idx = 0;
-  int count = 0, tmp;
-  deck_t * ans = NULL;
-  card_t *p;
-  char * sub;
-  if(strlen(str) == 0)
-    return NULL;
-  ans = malloc(sizeof(*ans));
-  ans->cards = NULL;
-  ans->n_cards = 0;
-  while((sub = getsub(str, idx)) != NULL){
-    count++;
-    tmp = strlen(sub);
-    idx = tmp + 1;
-    if(sub[0] == '?'){
-      tmp = atoi(sub+1);//index in fc
-      p = add_empty_card(ans);
-      add_future_card(fc, idx, p);
+  int len=strlen(str);
+  deck_t *ans=malloc(sizeof(*ans));
+  ans->cards=NULL;
+  ans->n_cards=0;
+  for(int i=0;i<len-1;i++){
+    if ((str[i] == '\n') || (str[i] == ' ')){
+      continue;
     }
     else{
-      add_card_to(ans, card_from_letters(sub[0], sub[1]));
-    }
+      if(str[i]=='?'){
+	i++;
+	char num[strlen(str)];
+	int n = 0;
+	while (!((str[i] == '\n') || (str[i] == ' '))){
+	  num[n] = str[i];
+	  i++;
+	  n++;
+	}
+	num[n] = '\0';
+	add_future_card(fc, atoi(num), add_empty_card(ans));
+      }
+      else{
+	card_t card=card_from_letters(str[i],str[i+1]);
+	add_card_to(ans,card);
+	i++;
+      }
+    }}
+  if(ans->n_cards<5){
+    fprintf(stderr,"Less than 5 cards\n");
+    return NULL;
   }
   return ans;
 }
@@ -65,7 +51,6 @@ deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
   deck_t ** ans = NULL;
   while(getline(&line, &sz, f) > 0){
     ans = realloc(ans, (*n_hands + 1) * sizeof(*ans));
-    stripNewLine(line);
     ans[*n_hands] = hand_from_string(line , fc);
     *n_hands = *n_hands + 1;
   }
