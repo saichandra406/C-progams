3775493c07fd169f94cd05a86d7212563e69efee
Coursera Learner <nobody@nowhere.nul>
2020-12-07 06:37:56 +0000

    did assignment c4pjr2

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -18,11 +18,32 @@ void copyCard(card_t * dest, card_t * src){
 }
 
 void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
-  deck_t cur_deck = fc->decks[index];
-  if(isEmptyCard(cur_deck.cards[0])){
+  deck_t cur_deck;
+  if(fc == NULL){
+    fc = malloc(sizeof(*fc));
+    fc->n_decks = 0;
+    fc->decks = NULL;
+  }
+  if(fc->n_decks <= index){
+    fc->decks = realloc(fc->decks, (index + 1) * sizeof(*fc->decks));
+    fc->n_decks = index + 1;
+    fc->decks[index] = malloc(sizeof(*fc->decks[index]));
+    fc->decks[index]->n_cards = 0;
+    fc->decks[index]->cards = NULL:
+  }
+  cur_deck = fc->decks[index];
+  cur_deck->n_cards++;
+  cur_deck->cards = realloc(cur_deck->cards, \
+			    cur_deck->n_cards * sizeof(*cur_deck->cards));
+  cur_deck->cards[cur_deck->n_cards - 1] = ptr;
+}
+
+void replace_future_card(future_cards_t * fc, size_t index, card_t * ptr) {
+  deck_t * cur_deck = fc->decks[index];
+  if(cur_deck->n_cards > 0 && isEmptyCard(cur_deck->cards[0])){
     //current cards are not replaced.
-    for(size_t j = 0; j < cur_deck.n_cards; j++){
-      copyCard(cur_deck.cards[j], ptr);
+    for(size_t j = 0; j < cur_deck->n_cards; j++){
+      copyCard(cur_deck->cards[j], ptr);
     }
   }
   //if its not empy card do nothing
@@ -38,7 +59,7 @@ void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
   }
   else{
     for(size_t i = 0; i < deck->n_cards; i++){
-      add_future_card(fc, i,deck->cards[i]);
-	}
+      replace_future_card(fc, i,deck->cards[i]);
+    }
   }
 }
