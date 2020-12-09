c68dace4e9267bc15cc85c6031b156b2add7593b
Coursera Learner <nobody@nowhere.nul>
2020-12-09 16:01:30 +0000

    main partial 1

--- c4prj3_finish/main.c
+++ c4prj3_finish/main.c
@@ -12,5 +12,19 @@
 
 int main(int argc, char ** argv) {
   //YOUR CODE GOES HERE
+  FILE *f;
+
+  if(!(argc == 2 || argc == 3)){
+    fprintf(stderr, "Does not have 1 or 2 arguments\n");
+    return EXIT_FAILURE;
+  }
+  
+  f = fopen(argv[1], "r");
+  if(f == NULL){
+    fprintf(stderr, "Cannot open file: %s\n", argv[1]);
+    return EXIT_FAILURE;
+  }
+
+  fclose(f);
   return EXIT_SUCCESS;
 }
